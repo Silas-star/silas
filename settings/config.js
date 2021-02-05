d0f7f1856af1e67ebb9224bef2391fcf8386d350
Silas-star <kauavefago20@gmail.com>
2021-02-05 15:48:20 -0300

    First commit

---
+++ settings/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'SilBOT',
+        ownerName: 'Sil',
+        youtube: 'YOUTUBE_LINK',
+        instagram: 'INSTAGRAM_LINK',
+}
---
+++ src/paulomenu.js
@@ -0,0 +1,12 @@
+const silmenu = (prefix, pushname) => {
+    return `◪ *Comandos do 🌊⃟ꦿ⸼Bot♤Persohard◊⸵░⃟🦈*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.silmenu = silmenu
